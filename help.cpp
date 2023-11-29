ba9541f17d9daa098ec91ad01741db595be88386
archivehaze <124225054+archivehaze@users.noreply.github.com>
2023-11-29 17:08:40 +0000

    Add files via upload

---
+++ help.cpp
@@ -0,0 +1,19 @@
+// file to develop help widgets to provide accessibility
+
+void Help::posting(){
+    /* to include:
+        when can you post
+        posting restrictions
+        what happens if you have no wifi
+        can i delete a post
+    */
+
+    
+}
+
+void Help:friends(){
+    /* to include:
+        how to add friends
+        can all friends see my post
+    */
+}
